270a0f3ed2e3388add913ab0e001117efb407e95
WeebNetsu <stephenvdw7777@gmail.com>
2023-02-11 15:03:51 +0200

    Initial commit

---
+++ 1. Hello World/main.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main // same name as the project
+{
+    class MyProgram // class can be called anything 
+    {
+        static void Main(string[] args) // main program
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
