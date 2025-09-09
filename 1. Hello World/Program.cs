3364153b6cb37c17bd41930cc91249f380fc38f5
Stephen <stephenvdw7777@gmail.com>
2025-09-09 09:02:24 +0200

    Update first lesson

---
+++ 1. Hello World/Program.cs
@@ -0,0 +1,26 @@
+﻿// you can generate a new console program with `dotnet new console -o MyApp` on Linux
+
+// you don't need to specify a namespace in such a basic program, you can just start writing
+// Console.WriteLine("Hello, World!");
+
+// 2nd option is to specify a namespace with a main program inside it
+namespace _1._Hello_World // project name (however will work fine if something random)
+{
+    class Program
+    {
+        static void Main(string[] args) // main method similar to C++ and Java
+        {
+            /* 
+            Multi
+            line
+            comment
+             */
+            Console.WriteLine("Hello, World!"); // print to console (; is required)
+
+            // if not using a terminal (like we are on Linux) and you console is just opening and closing
+            // (looking at you windows), then you can attach the below so it waits for input before closing
+            // the window
+            // Console.ReadLine();
+        }
+    }
+}
